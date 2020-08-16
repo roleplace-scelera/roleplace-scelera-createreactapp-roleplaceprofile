9bfdd4ccdc1affbe0e67845986195ff08edac89d
Rachel <rpetitto19@gmail.com>
2020-08-16 13:08:37 -0400

    Build scripts and stuff

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            This is CreateReactApp for Roleplace Profiles! To get started run: <br/>
+            <code>
+              git clone https://github.com/roleplace-scelera/createreactapp-roleplaceprofile.git <br/>
+              yarn install <br/>
+              yarn start <br/>
+            </code>
+            To get your profile code, at any time, you can run: <br/>
+            <code>
+              yarn build <br/>
+            </code>
+            Copy the contents of build/index.html to your Roleplace profile. (It is minified).  <br/>
+            That's it! Thank you for deciding to join us on Roleplace, and enjoy the node support! ;)
+          </p>
+          <a
+              className="App-link"
+              href="https://github.com/roleplace-scelera"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Check out our Roleplace Templates
+          </a>
+        </header>
+      </div>
   );
 }
 
